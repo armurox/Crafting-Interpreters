ab96b4ced877942c686de42846666367296baf07
Arman Jasuja <arman_jasuja@yahoo.com>
2023-11-20 00:52:40 +0400

    Tested delete node function

--- chapter_1/challenges/test.c
+++ chapter_1/challenges/test.c
@@ -32,10 +32,37 @@ void test_add_front()
     head = NULL;
 }
 
+void test_delete_node()
+{
+    Node *head = create_node("Test");
+    Node *temp = head;
+    if (head == NULL)
+    {
+        printf("Heap Overflow\n");
+    }
+
+    add_front(&head, "Test_1");
+    assert(strcmp(head -> word, "Test_1") == 0);
+    assert(head -> next == temp);
+    add_front(&head, "Test_2");
+    add_front(&head, "Test_3");
+    delete_node(&head, "Test_3");
+    assert(strcmp(head -> word, "Test_2") == 0);
+    assert(strcmp((head -> next) -> word, "Test_1") == 0);
+    delete_node(&head, "Test_1");
+    assert(strcmp(head -> word, "Test_2") == 0);
+    assert(strcmp((head -> next) -> word, "Test") == 0);
+    assert(head -> previous == NULL);
+    assert((head -> next) -> next == NULL);
+    _terminate(head);
+    head = NULL;
+}
+
 int main()
 {
     printf("Starting tests...\n");
     test_create_node();
     test_add_front();
+    test_delete_node();
     printf("All tests passed!\n");
 }
